b0a9b66dcab6ee0253e9c77a568a18e4a798aef2
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-11-23 15:58:51 +0100

    Add render beforeEach

--- tests/Canvas.test.jsx
+++ tests/Canvas.test.jsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import Canvas from '../src/components/Canvas';
 
 describe('Canvas', () => {
+  beforeEach(() => render(<Canvas />));
+
   it('renders image', () => {
-    render(<Canvas />);
     expect(
       screen.getByAltText('Section of big park in Rollercoaster Tycoon'),
     ).toBeInTheDocument();
