33932ce784e021818a804498fc6a098123648dde
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-11-24 12:30:51 +0100

    Re-add menu

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -6,13 +6,55 @@ import { X, CircleDashed } from 'lucide-react';
 
 export default function Selector({ clickCoordinates, setSelectorVisible }) {
   const { x, y } = clickCoordinates;
-  const selectorSize = 75;
+
+  // Selector dimensions
+  const selectorIconSize = 75;
+  const selectorWrapperWidth = 150;
+  const selectorWrapperHeight = 200;
+  const rowGap = 10;
+  // Menu is below selector icon by default
+  let selectorMenuOrder = 1;
+
+  // Calculate offsets
+  let selectorWrapperTop = y - selectorIconSize / 2;
+  const selectorWrapperLeft = x - selectorWrapperWidth / 2;
+
+  // Put the menu on top when user clicks near the bottom of the image,
+  // preventing the menu from appearing outside the image
+  if (y > 1000) {
+    selectorMenuOrder = -1;
+    selectorWrapperTop -= selectorWrapperHeight + rowGap; // Recalculate offset
+  }
+
   return (
     <div
-      className="selector"
-      style={{ top: y - selectorSize / 2, left: x - selectorSize / 2 }}
+      className="selector-wrapper"
+      style={{
+        top: selectorWrapperTop,
+        left: selectorWrapperLeft,
+        rowGap,
+      }}
     >
-      <CircleDashed size={selectorSize} strokeWidth={2.5} />
+      <CircleDashed
+        className="selector-icon"
+        size={selectorIconSize}
+        strokeWidth={2.5}
+      />
+      <div
+        className="selector-menu"
+        style={{ height: selectorWrapperHeight, order: selectorMenuOrder }}
+      >
+        <button
+          type="button"
+          aria-label="Close selector"
+          className="selector-close generic-button"
+          onClick={() => setSelectorVisible(false)}
+        >
+          <X />
+        </button>
+        <div className="item-1">One</div>
+        <div className="item-2">Two</div>
+      </div>
     </div>
   );
 }
