65f374e95c760688ae1fa4b710261d8fe12f1296
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-11-25 12:45:43 +0100

    Preload icons

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import preloadIcons from '../utils/preloadIcons';
 import Canvas from '../components/Canvas';
 
 import '../components/styles/App.css';
 
 function App() {
+  preloadIcons();
+
   return <Canvas />;
 }
 
