ccf822e526b72582edd9dd273fe7548a5a988ad8
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-11-27 12:17:07 +0100

    Add credit

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -7,7 +7,15 @@ import '../components/styles/App.css';
 function App() {
   preloadIcons();
 
-  return <Canvas />;
+  return (
+    <>
+      <Canvas />
+      <div className="credit">
+        <a href="https://www.youtube.com/watch?v=BR3kGw_FMOM">Image credit.</a>
+        {'  '} Used with permission.
+      </div>
+    </>
+  );
 }
 
 export default App;
