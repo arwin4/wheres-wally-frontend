49acb51296b11e43131d0d961addd31e0660e5ad
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-12-19 17:54:56 +0100

    Position InfoBar on top on narrow screens

--- src/components/InfoBar.jsx
+++ src/components/InfoBar.jsx
@@ -7,47 +7,45 @@ import { Icon } from '@iconify/react';
 export default function InfoBar({ walliesFound }) {
   return (
     <div className="infobar">
-      <div className="track-wallies">
-        <div className="wally-wrapper">
-          <img
-            className="tracked-wally"
-            src="assets/wallies/fountain.png"
-            alt="Fountain"
+      <div className="wally-wrapper">
+        <img
+          className="tracked-wally"
+          src="assets/wallies/fountain.png"
+          alt="Fountain"
+        />
+        {walliesFound.includes('fountain') && (
+          <Icon
+            className="tracked-wally-overlay"
+            icon="pixelarticons:radio-on"
+            color="white"
+            height={75}
           />
-          {walliesFound.includes('fountain') && (
-            <Icon
-              className="tracked-wally-overlay"
-              icon="pixelarticons:radio-on"
-              color="white"
-              height={75}
-            />
-          )}
-        </div>
-        <div className="wally-wrapper">
-          <img src="assets/wallies/slide.png" alt="Slide" />
-          {walliesFound.includes('slide') && (
-            <Icon
-              className="tracked-wally-overlay"
-              icon="pixelarticons:radio-on"
-              color="white"
-              height={75}
-            />
-          )}
-        </div>
-        <div className="wally-wrapper">
-          <img
-            src="assets/wallies/lonely-island.png"
-            alt="Lonely island with person stranded on it"
+        )}
+      </div>
+      <div className="wally-wrapper">
+        <img src="assets/wallies/slide.png" alt="Slide" />
+        {walliesFound.includes('slide') && (
+          <Icon
+            className="tracked-wally-overlay"
+            icon="pixelarticons:radio-on"
+            color="white"
+            height={75}
           />
-          {walliesFound.includes('lonely island') && (
-            <Icon
-              className="tracked-wally-overlay"
-              icon="pixelarticons:radio-on"
-              color="white"
-              height={75}
-            />
-          )}
-        </div>
+        )}
+      </div>
+      <div className="wally-wrapper">
+        <img
+          src="assets/wallies/lonely-island.png"
+          alt="Lonely island with person stranded on it"
+        />
+        {walliesFound.includes('lonely island') && (
+          <Icon
+            className="tracked-wally-overlay"
+            icon="pixelarticons:radio-on"
+            color="white"
+            height={75}
+          />
+        )}
       </div>
     </div>
   );
